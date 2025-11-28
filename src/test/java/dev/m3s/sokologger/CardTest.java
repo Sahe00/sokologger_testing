e796492f0317ca88f3d98f7ae184a8a79b377122
Santeri Heikkinen <saheikki20@student.oulu.fi>
2025-11-28 20:01:10 +0200

    add cardtest.java

--- src/test/java/dev/m3s/sokologger/CardTest.java
+++ src/test/java/dev/m3s/sokologger/CardTest.java
@@ -1,5 +1,68 @@
 package dev.m3s.sokologger;
 
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
 public class CardTest {
+    
+    @AfterEach
+    void resetCardTest() {
+        System.clearProperty("output.simple");
+    }
+
+    // CONSTRUCTOR TESTS
+
+    @Test
+    void testCardCreation() throws SokoException {
+        Card card = new Card(Suite.SPADES, 4);
+        assertNotNull(card);
+        assertEquals(Suite.SPADES, card.getSuite());
+        assertEquals(4 , card.getValue());
+    }
+
+    @Test
+    void testCardValueTooLow() {
+        var e = assertThrows(SokoException.class, 
+            () -> new Card(Suite.SPADES, 0));
+        
+        assertTrue(e.getMessage().contains("between 1 and 13"));
+    }
+
+    @Test
+    void testCardValueTooHigh() {
+        var e = assertThrows(SokoException.class, 
+            () -> new Card(Suite.HEARTS, 14));
+
+        assertTrue(e.getMessage().contains("between 1 and 13"));
+    }
+
+    @Test
+    void testCardValueNegative() {
+        var e = assertThrows(SokoException.class, 
+            () -> new Card(Suite.DIAMONDS, -3));
+        
+        assertTrue(e.getMessage().contains("between 1 and 13"));
+    }
+
+    // BOUNDARY VALUE TESTS / SOME TESTED ABOVE
+
+    @Test
+    void testValueAtLowerBoundaryValid() throws SokoException {
+    Card card = new Card(Suite.HEARTS, 1);
+    assertEquals(1, card.getValue());
+    }
+
+    @Test
+    void testValueAtUpperBoundaryValid() throws SokoException {
+    Card card = new Card(Suite.CLUBS, 13);
+    assertEquals(13, card.getValue());
+    }
+
+    // GETTER/SETTER TESTS
+
     
 }
